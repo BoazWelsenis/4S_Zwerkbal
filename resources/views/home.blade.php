e963c27c6cb1e000199595607da0c2bd5568ad89
BoazWelsenis <D281029@edu.rocwb.nl>
2021-09-21 12:14:35 +0200

    Centreren Tekst - Styling Dashboard Pagina
    Tekst in het midden uitlijnen.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,14 +15,14 @@
     <p>Zwerkbal (Engels: Quidditch) is een sport voor tovenaars uit de Harray Potter boeken van Joanne Rowling. In de wereld van Harray Potter is Zwerkbal een populaire sport: veel mensen praten erover mee en zijn zeer enthousiast als er een groot toernooi wordt gespeeld. Ook heeft iedere heks of tovenaar wel een favoriet Zwerkbalteam. Het idee van Zwerkbal lijkt te zijn gebaseerd op het non-fictieve horseball.</p>
 
     <div class="index-grid">
-        <div class="span-2-x yellow-border font-size-index">
+        <div class="span-2-x yellow-border font-size-index text-center">
             <p>Aantal aankomende toernooien:</p>
             <h3>5</h3>
         </div>
         <div class="snitch-img span-2-xy yellow-border">
             <img src="img/snitch.png" alt="snitch img">
         </div>
-        <div class="span-2-x yellow-border font-size-index">
+        <div class="span-2-x yellow-border font-size-index text-center">
             <p>Totaal aantal teams:</p>
             <h3>26</h3>
         </div>
